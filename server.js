86103f14e2f85abf2cc9c0405fe6094119cc0c50
semir <seper56@gmail.com>
2023-02-13 20:02:25 -0500

    refactoring and setting up

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./Develop/routes');
+const routes = require('./routes');
 // import sequelize connection
 
 const app = express();
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// sync Sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
